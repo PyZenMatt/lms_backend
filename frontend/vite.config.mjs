452511749e18a4ceacd99e1a30447f901f2f1795
PyZenMatt <matteoricci12@outlook.it>
2025-08-17 13:38:00 +0200

    package mancanti

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import { splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
   server: {
@@ -70,8 +69,7 @@ export default defineConfig({
         ].filter(Boolean)
       }
     }), 
-    jsconfigPaths(),
-    splitVendorChunkPlugin()
+    jsconfigPaths()
   ],
   build: {
     // Production build optimizations
